051e82816f7ad774b322168a03b996bca5eba43a
Mikolaj <mikolaj.jecz@gmail.com>
2025-04-22 13:27:13 +0200

    about-us finished, offer created

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
 const heroBtn = document.querySelector('.header__hero-button')
 const burgerBtn = document.querySelector('.nav-mobile__burger-btn')
 const navMenu = document.querySelector('.nav-mobile__items')
-
-burgerBtn.addEventListener('click',()=>{
-    burgerBtn.classList.toggle('is-active')
-    navMenu.classList.toggle('nav-mobile__items--active')
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-	setInterval(heroBtnAnimation, 7000)
-})
+const menuItems = document.querySelectorAll('.nav__item')
 
 const heroBtnAnimation = () => {
 	heroBtn.classList.add('button-animation')
@@ -18,3 +10,19 @@ const heroBtnAnimation = () => {
 		heroBtn.classList.remove('button-animation')
 	}, 1000) // Usunięcie klasy po 1 sekundzie
 }
+
+
+menuItems.forEach(item =>{
+	item.addEventListener('click', () => {
+		navMenu.classList.remove('nav-mobile__items--active')
+	})
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+	setInterval(heroBtnAnimation, 7000)
+})
+
+burgerBtn.addEventListener('click', () => {
+	burgerBtn.classList.toggle('is-active')
+	navMenu.classList.toggle('nav-mobile__items--active')
+})
